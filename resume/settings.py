a3a7c38878168e0e70f63be00fb3e6b95f9e103f
batbruce15 <karim.battou20@gmail.com>
2023-06-11 14:32:22 +0200

    ready to deploy

--- resume/settings.py
+++ resume/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-6@+*c$twjs63ba4rvobc19okkab^#nzd%flb$q^qa7m8-wv@u*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
